ec8b15bd7a29ed3eefd10e81f6eb4a4fbfbad4e6
orcade <51912585+orcade@users.noreply.github.com>
2019-06-18 13:21:31 +0200

    ajout tâches
    modification ajout collaborateur virginie

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 /*Cedric veut tu bien me faire le code de question pour un champion stp!!!! MERCI ;-) */
 const cedric = champion;
-
 /*fin code js Cedric*/
+/*debut code js virginie*/
+
+/*fin code virginie*/
